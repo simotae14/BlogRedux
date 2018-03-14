e8a69be80c0984e82793e3c4de1122b785027005
Simone Taeggi <simone.taeggi@leovegas.com>
2018-03-14 15:39:31 +0100

    Creazione Form Nuovo Post

---
+++ src/components/posts_new.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class PostsNew extends Component {
+  render() {
+    return (
+      <div>Form di Creazione</div>
+    );
+  }
+}
+
+export default PostsNew;
