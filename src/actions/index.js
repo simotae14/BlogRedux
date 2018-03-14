2035f0ab55a339b1e9f4b5fe75eead7243c04f2b
Simone Taeggi <simone.taeggi@leovegas.com>
2018-03-14 18:06:22 +0100

    Aggiunto l'action Creator per salvare il nuovo post invocando api

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
+export const CREATE_POST = 'CREATE_POST';
 
 // url api
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
@@ -8,10 +9,20 @@ const API_KEY = '?key=123';
 
 export function fetchPosts() {
   // definisco la Request
-  const request = axios.get(`${ROOT_URL}/posts${API_KEY}`)
+  const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
 
   return {
     type: FETCH_POSTS,
     payload: request
-  }
+  };
+}
+
+// action Creator per creare un post
+export function createPost(props) {
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, props);
+
+  return {
+    type: CREATE_POST,
+    payload: request
+  };
 }
--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
+import { createPost } from '../actions/index';
 
 class PostsNew extends Component {
   render() {
-    const { fields: { title, categories, content }, handleSubmit } = this.props;
+    const { fields: { title, categories, content } } = this.props;
+    const { handleSubmit } = this.props;
 
     return (
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(this.props.createPost)}>
         <h3>Crea Un Nuovo Post</h3>
         <div className="form-group">
           <label>Titolo</label>
@@ -26,7 +28,10 @@ class PostsNew extends Component {
   }
 }
 
+// connect: il primo argomento è mapStateToProps, il secondo mapDispatchToProps
+// reduxForm: il primo argomento è la configuarazione della form, il secondo mapStateToProps ed
+// il terzo è mapDispatchToProps
 export default reduxForm({
   form: 'PostsNewForm',
   fields: ['title', 'categories', 'content']
-})(PostsNew);
+}, null, { createPost })(PostsNew);
