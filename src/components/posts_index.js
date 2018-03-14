c5dab3b078201cc109c314fb9414719e670dece8
Simone Taeggi <simone.taeggi@leovegas.com>
2018-03-14 14:58:24 +0100

    Recupero dei Posts con i metodi Lifecycle

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,5 +1,17 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+// importo l'action creator
+import { fetchPosts } from '../actions/index';
 
-export default () => {
-    return <div>Elenco dei post.</div>;
+class PostsIndex extends Component {
+  componentWillMount() {
+    this.props.fetchPosts();
+  }
+  render() {
+    return (
+      <div>Elenco dei post</div>
+    );
+  }
 }
+
+export default connect(null, { fetchPosts })(PostsIndex);
