9f1be02ff8449f794ba8465ee36197985ade3836
Simone Taeggi <simone.taeggi@leovegas.com>
2018-03-14 17:33:09 +0100

    Aggiunto Redux Form per gestire la mia form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -3,20 +3,22 @@ import { reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
   render() {
+    const { fields: { title, categories, content }, handleSubmit } = this.props;
+
     return (
-      <form>
+      <form onSubmit={handleSubmit}>
         <h3>Crea Un Nuovo Post</h3>
         <div className="form-group">
           <label>Titolo</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" {...title} />
         </div>
         <div className="form-group">
           <label>Categorie</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" {...categories} />
         </div>
         <div className="form-group">
           <label>Titolo</label>
-          <textarea className="form-control" />
+          <textarea className="form-control" {...content} />
         </div>
         <button type="submit" className="btn btn-primary">Invia</button>
       </form>
