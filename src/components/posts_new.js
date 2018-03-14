2f42818a4ec0a214590532d3ab0d8bea3c223b69
Simone Taeggi <simone.taeggi@leovegas.com>
2018-03-14 16:42:19 +0100

    Creato il contenuto della Form per il Nuovo Post

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
   render() {
     return (
-      <div>Form di Creazione</div>
+      <form>
+        <h3>Crea Un Nuovo Post</h3>
+        <div className="form-group">
+          <label>Titolo</label>
+          <input type="text" className="form-control" />
+        </div>
+        <div className="form-group">
+          <label>Categorie</label>
+          <input type="text" className="form-control" />
+        </div>
+        <div className="form-group">
+          <label>Titolo</label>
+          <textarea className="form-control" />
+        </div>
+        <button type="submit" className="btn btn-primary">Invia</button>
+      </form>
     );
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostsNewForm',
+  fields: ['title', 'categories', 'content']
+})(PostsNew);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
 import PostsReducer from './reducer_posts';
+// importo il Reducer per la ReduxForm
+import { reducer as formReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
-  posts: PostsReducer
+  posts: PostsReducer,
+  form: formReducer
 });
 
 export default rootReducer;
