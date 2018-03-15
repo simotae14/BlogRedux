a85a886e83df03d02da73b8e4d5c0144b6df67ca
Simone Taeggi <simone.taeggi@leovegas.com>
2018-03-15 12:48:46 +0100

    Cancellare un Post

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -9,16 +9,20 @@ class PostsIndex extends Component {
     this.props.fetchPosts();
   }
 
+
   renderPosts() {
     return this.props.posts.map((post) => {
-      return (
-        <li className="list-group-item" key={post.id}>
-          <Link to={"posts/" + post.id} >
-            <span className="pull-xs-right">{post.categories}</span>
-            <strong>{post.title}</strong>
-          </Link>
-        </li>
-      );
+      if(post.title || post.categories){
+
+        return (
+          <li className="list-group-item" key={post.id}>
+            <Link to={"posts/" + post.id} >
+              <span className="pull-xs-right">{post.categories}</span>
+              <strong>{post.title}</strong>
+            </Link>
+          </li>
+        );
+      }
     });
   }
   render() {
--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchPost } from '../actions/index';
+import { fetchPost, deletePost } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsShow extends Component {
   componentWillMount() {
     this.props.fetchPost(this.props.params.id);
+  }
+
+  onDeleteClick() {
+    this.props.deletePost(this.props.params.id);
   }
 
   render() {
@@ -14,6 +19,12 @@ class PostsShow extends Component {
     }
     return (
       <div>
+        <Link to="/">Torna all&#39;Indice</Link>
+        <button
+          className="btn btn-danger pull-xs-right"
+          onClick={this.onDeleteClick.bind(this)}>
+          Cancella il Post
+        </button>
         <h3>{post.title}</h3>
         <h6>Categorie: {post.categories}</h6>
         <p>{post.content}</p>
@@ -28,4 +39,4 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps, { fetchPost })(PostsShow);
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow);
