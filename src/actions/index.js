5da1e130b275bd85568b0229979dfcf535e9b1bf
Simone Taeggi <simone.taeggi@leovegas.com>
2018-03-15 11:57:19 +0100

    Creazione Action Creator e metodi del Reducer e nel Component per il fetching del Post

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const CREATE_POST = 'CREATE_POST';
+export const FETCH_POST = 'FETCH_POST';
 
 // url api
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
@@ -23,6 +24,16 @@ export function createPost(props) {
 
   return {
     type: CREATE_POST,
+    payload: request
+  };
+}
+
+export function fetchPost(id) {
+  // definisco la Request
+  const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
+  return {
+    type: FETCH_POST,
     payload: request
   };
 }
